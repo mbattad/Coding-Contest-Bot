8c042587cce221884974ccd8fefe1a4e99b7583f
mbattad <35310158+mbattad@users.noreply.github.com>
2022-08-16 17:06:49 -0500

    quick rename lol

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login("SECRET");
