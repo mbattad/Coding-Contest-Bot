f4e00de06227e936156e316d1bbd25672472a1c5
mbattad <35310158+mbattad@users.noreply.github.com>
2022-08-16 16:48:30 -0500

    added test code

---
+++ testScript.js
@@ -0,0 +1,17 @@
+const { Client, Intents } = require("discord.js");
+// The Client and Intents are destructured from discord.js, since it exports an object by default. Read up on destructuring here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login("SECRET");
