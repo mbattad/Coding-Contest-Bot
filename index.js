cad82c7fc2369e7914cfae29a48a72330f8a31f7
mbattad <35310158+mbattad@users.noreply.github.com>
2022-08-16 18:09:55 -0500

    added gitignore & config

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const { REST, Routes } = require('discord.js');
+const config = require('./config.json');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken('WhTubR1Ai2bShR-O-oYtu3Z0HxxliKpj');
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(config.clientId), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
@@ -6,10 +30,12 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 
-client.on("messageCreate", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
   }
 });
 
-client.login("SECRET");
+client.login(config.token);
